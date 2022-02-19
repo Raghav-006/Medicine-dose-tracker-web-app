6f8cf96d5b3d873413a5c7c6cc7a2dc16c4643e2
Gilbert Mavhungu <ronewagilbert3@gmail.com>
2022-02-19 22:48:53 +0200

    form

--- frontend/src/pages/NewMedicine.js
+++ frontend/src/pages/NewMedicine.js
@@ -10,7 +10,8 @@ export default function NewMedicine() {
     e.preventDefault()
     if (!name || !dosage || !frequency) return;
       const medicine = { name, dosage, frequency };
-    await axios.post('',medicine,{withCredentials:true})
+    //await axios.post('',medicine,{withCredentials:true})
+    console.log(medicine)
 
   }
   return (
@@ -19,30 +20,39 @@ export default function NewMedicine() {
         <h1 className='h2'>Enter new forms</h1>
       </div>
       <div className='formss'>
-          <form className="row g-3 needs-validation" novalidate onSubmit={Meds}>
-            <div class="col-md-4">
-              <label for="validationCustom01" class="form-label">Medicine name</label>
+          <form className="row g-3 needs-validation" onSubmit={Meds}>
+            <div className="col-md-4">
+              <label htmlFor="validationCustom01" className="form-label">Medicine name</label>
               <input type="text" className="form-control" id="validationCustom01" value={name} onChange={(e)=>setName(e.target.value)} required />
               <div className="valid-feedback">
                 Looks good!
               </div>
             </div>
-            <div className="col-md-4">
-              <label for="validationCustom02" className="form-label">Dosage</label>
+            <div className="col-auto">
+              <label htmlFor="validationCustom02" className="form-label">Dosage</label>
               <input type="text" className="form-control" id="validationCustom02" value={dosage} onChange={(e)=>setDosage(e.target.value)} required />
               <div className="valid-feedback">
                 Looks good!
               </div>
             </div>
-            <div className="col-md-4">
-              <label for="validationCustomUsername" className="form-label">Frequency</label>
+            {/*<div className="col-md-4">
+              <label htmlFor="validationCustomUsername" className="form-label">Frequency</label>
                 <input type="text" className="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" onChange={(e)=>setFrequency(e.target.value)} required />
                 <div className="valid-feedback">
                   Please choose a username.
               </div>
+            </div>*/}
+            <div className="col-auto">
+              <label className="form-label" htmlFor="autoSizingSelect">Frequency</label>
+              <select className="form-select" id="autoSizingSelect" onSelect={(e)=>setFrequency(e.target.value)}>
+                <option muted>Choose...</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
             </div>
-            <div class="col-12">
-              <button class="btn btn-primary" type="submit">Submit form</button>
+            <div className="col-12">
+              <button className="btn btn-primary" type="submit">Submit form</button>
             </div>
           </form>
         </div>
