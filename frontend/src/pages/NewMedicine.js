9ebc6c8b34e4e51a77bfaeab577918676e172089
Gilbert Mavhungu <ronewagilbert3@gmail.com>
2022-02-19 18:40:45 +0200

    submit from e

--- frontend/src/pages/NewMedicine.js
+++ frontend/src/pages/NewMedicine.js
@@ -11,6 +11,7 @@ export default function NewMedicine() {
     if (!name || !dosage || !frequency) return;
       const medicine = { name, dosage, frequency };
     await axios.post('',medicine,{withCredentials:true})
+
   }
   return (
     <Wrapper>
@@ -20,27 +21,24 @@ export default function NewMedicine() {
       <div className='formss'>
           <form className="row g-3 needs-validation" novalidate onSubmit={Meds}>
             <div class="col-md-4">
-              <label for="validationCustom01" class="form-label">First name</label>
-              <input type="text" className="form-control" id="validationCustom01" value="Mark" required />
+              <label for="validationCustom01" class="form-label">Medicine name</label>
+              <input type="text" className="form-control" id="validationCustom01" value={name} onChange={(e)=>setName(e.target.value)} required />
               <div className="valid-feedback">
                 Looks good!
               </div>
             </div>
             <div className="col-md-4">
-              <label for="validationCustom02" className="form-label">Last name</label>
-              <input type="text" className="form-control" id="validationCustom02" value="Otto" required />
+              <label for="validationCustom02" className="form-label">Dosage</label>
+              <input type="text" className="form-control" id="validationCustom02" value={dosage} onChange={(e)=>setDosage(e.target.value)} required />
               <div className="valid-feedback">
                 Looks good!
               </div>
             </div>
             <div className="col-md-4">
-              <label for="validationCustomUsername" className="form-label">Username</label>
-              <div className="input-group">
-                <span className="input-group-text" id="inputGroupPrepend">@</span>
-                <input type="text" className="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required />
-                <div className="invalid-feedback">
+              <label for="validationCustomUsername" className="form-label">Frequency</label>
+                <input type="text" className="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" onChange={(e)=>setFrequency(e.target.vakue)} required />
+                <div className="valid-feedback">
                   Please choose a username.
-                </div>
               </div>
             </div>
             <div class="col-12">
