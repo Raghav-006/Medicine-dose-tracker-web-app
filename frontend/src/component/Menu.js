22df61813ccc299513eb8a2984ceff04321e9ce6
ronewa <ronewamavhungu@gmail.com>
2022-02-21 22:54:59 +0200

    clean mune

--- frontend/src/component/Menu.js
+++ frontend/src/component/Menu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import { BarChart2, Camera, Home, Layers, PlusCircle } from 'react-feather';
-import {FaPenAlt, FaPencilAlt} from 'react-icons/fa';
+//import {FaPenAlt, FaPencilAlt} from 'react-icons/fa';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPills } from '@fortawesome/free-solid-svg-icons'
 
@@ -42,16 +42,16 @@ export default function Menu() {
               Orders
             </Link>
           </li>
-          <li className="nav-item">
+          {/*<li className="nav-item">
             <Link to="/users" className="nav-link">
               <span><FaPencilAlt className="feather"/></span>
               FaPenAlt <FaPenAlt className='feather'/>
             </Link>
-          </li>
+          </li>*/}
         </ul>
         <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
           <span>Saved reports</span>
-          <Link className="link-secondary" to="#" aria-label="Add a new report">
+          <Link className="link-secondary" to="#!" aria-label="Add a new report">
             <span data-feather="plus-circle"><PlusCircle className='feather'/></span>
           </Link>
         </h6>
