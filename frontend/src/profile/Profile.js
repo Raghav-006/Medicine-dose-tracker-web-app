11c603afb9dbaf256076871884b8d214ee518037
Gilbert Mavhungu <ronewagilbert3@gmail.com>
2022-02-23 13:37:45 +0200

    prrofilr form

--- frontend/src/profile/Profile.js
+++ frontend/src/profile/Profile.js
@@ -12,9 +12,7 @@ export default function Profile() {
   return (
     <Wrapper>
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
-        {/*<h1 className='h2 text-muted'>Enter new forms</h1>*/}
       </div>
-        <p>Profile page</p>
         <div className='container'>
           <div className='row'>
             <div className='col-md-6'>
