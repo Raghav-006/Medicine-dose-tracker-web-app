9c23305fe4e9e2b4742c835657124dd0e983e36c
ronewa <ronewamavhungu@gmail.com>
2022-02-23 14:36:58 +0200

    submit

--- frontend/src/profile/Profile.js
+++ frontend/src/profile/Profile.js
@@ -15,15 +15,51 @@ export default function Profile() {
       </div>
         <div className='container'>
           <div className='row'>
-            <div className='col-md-6'>
+            <div className='col-md-8'>
               <form onSubmit={handleSubmit(onSubmit)}>
-                <div className='form-outline mb-4'>
-                  <input type={'text'} {...register("name")} className={'form-control'} />
+                <div className="row">
+                  <div className="col">
+                    <div className='form-outline mb-4'>
+                      <input type={'text'} {...register("name",{required:true})} className={'form-control'} />
+                      {errors.name && <span>This field is required</span>}
+                    </div>
+                  </div>
+                  <div className="col">
+                    <div className='form-outline mb-4'>
+                      <input type={"text"} {...register("email", {required:true})} className={'form-control'} />
+                      {errors.email && <span>This field is required</span>}
+                    </div>
+                  </div>
+                  <div className="col-12 mb-4">
+                    <label for="inputAddress" className="form-label">Address</label>
+                    <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" {...register("address",{required:true})} />
+                    {errors.address && <span>This field is required</span>}
+                  </div>
                 </div>
-                <div className='form-outline mb-4'>
-                  <input type={"text"} {...register("email")} className={'form-control'} />
+                <div className='row'>
+                  <div className="col-md-6 mb-4">
+                    <label for="inputCity" className="form-label">City</label>
+                    <input type="text" className="form-control" id="inputCity" {...register('city',{required:true})}/>
+                    {errors.city && <span>This field is required</span>}
+                  </div>
+                  <div className="col-md-4 mb-4">
+                    <label for="inputState" className="form-label">State</label>
+                    <select id="inputState" className="form-select" {...register("gender",{required: true})}>
+                      <option value='' className="text-muted">Choose...</option>
+                      <option value=''>...</option>
+                      <option value="female">female</option>
+                      <option value="male">male</option>
+                      <option value="other">other</option>
+                    </select>
+                      {errors.gender && <span>This field is required</span>}
+                  </div>
+                  <div className="col-md-2 mb-4">
+                    <label for="inputZip" className="form-label">Zip</label>
+                    <input type="text" className="form-control" id="inputZip" {...register('Zipcode',{required:true})}/>
+                    {errors.Zipcode && <span>Zip code is required</span>}
+                  </div>
                 </div>
-                <input type={'submit'} className="btn btn-primary btn-block" />
+                <input type={'submit'} className="btn btn-primary" value="Save" />
                 <input style={{ display: "block", marginTop: 20 }} type="button" className="d-none" onClick={() => reset()}/>
               </form>
             </div>
