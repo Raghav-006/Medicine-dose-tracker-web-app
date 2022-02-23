99cc345c36cfcdb13038b7888c2bd92eed69a473
Gilbert Mavhungu <ronewagilbert3@gmail.com>
2022-02-23 18:04:34 +0200

    fix chart

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -10,45 +10,49 @@ const Dashboard =()=> {
   const ctx = $('#myChart');
 const ctx = 'myChart';*/}
 
-  const myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
+const myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday'
+    ],
+    datasets: [{
+      data: [
+        15339,
+        21345,
+        18483,
+        24003,
+        23489,
+        24092,
+        12034
       ],
-      datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
+      lineTension: 0,
+      backgroundColor: 'transparent',
+      borderColor: '#007bff',
+      borderWidth: 4,
+      pointBackgroundColor: '#007bff'
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: false
+        }
       }]
     },
-    options: {
-      onClick: (e) => {
-        const canvasPosition = getRelativePosition(e, chart);
-  
-        // Substitute the appropriate scale IDs
-        //const dataX = chart.scales.x.getValueForPixel(canvasPosition.x);
-        //const dataY = chart.scales.y.getValueForPixel(canvasPosition.y);
-      }
+    legend: {
+      display: false
     }
-  });
+  }
+})
+();
 
   return (
     <Wrapper>
