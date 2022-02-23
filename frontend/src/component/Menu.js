0c558603226785747d246d6f145c70e5ca391acb
Gilbert Mavhungu <ronewagilbert3@gmail.com>
2022-02-23 13:15:54 +0200

    profile page

--- frontend/src/component/Menu.js
+++ frontend/src/component/Menu.js
@@ -1,61 +1,55 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import { BarChart2, User, Home, Layers, PlusCircle } from 'react-feather';
+import { BarChart2, Users, Home, Layers, PlusCircle } from 'react-feather';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPills } from '@fortawesome/free-solid-svg-icons'
 
 export default function Menu() {
   return (
     <>
-    <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div className="position-sticky pt-3">
-        <ul className="nav flex-column">
-          <li className="nav-item">
-            <Link to="/dashboard" active='true' className="nav-link" aria-current="page">
-              <span><Home className='feather'/></span>
-              Dashboard
+      <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+        <div className="position-sticky pt-3">
+          <ul className="nav flex-column">
+            <li className="nav-item">
+              <Link to="/dashboard" active='true' className="nav-link" aria-current="page">
+                <span><Home className='feather'/></span>
+                Dashboard
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/medicine" className="nav-link">
+                <span data-feather="file"></span>
+                <FontAwesomeIcon icon={faPills} className='feather'/>
+                Medicine 
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/reports">
+                  <BarChart2 className='feather'/>
+                Reports
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="#">
+                  <Layers className='feather'/>
+                Integrations
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/profile" className="nav-link">
+                <span><Users className="feather"/></span>
+                Profile
+              </Link>
+            </li>
+          </ul>
+          <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <span>Saved reports</span>
+            <Link className="link-secondary" to="#!" aria-label="Add a new report">
+              <span data-feather="plus-circle"><PlusCircle className='feather'/></span>
             </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/medicine" className="nav-link">
-              <span data-feather="file"></span>
-              <FontAwesomeIcon icon={faPills} className='feather'/>
-              Medicine 
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/reports">
-                <BarChart2 className='feather'/>
-              Reports
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="#">
-                <Layers className='feather'/>
-              Integrations
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/profile" className="nav-link">
-              <span><User className="feather"/></span>
-              Profile
-            </Link>
-          </li>
-          {/*<li className="nav-item">
-            <Link to="/users" className="nav-link">
-              <span><FaPencilAlt className="feather"/></span>
-              FaPenAlt <FaPenAlt className='feather'/>
-            </Link>
-          </li>*/}
-        </ul>
-        <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          <span>Saved reports</span>
-          <Link className="link-secondary" to="#!" aria-label="Add a new report">
-            <span data-feather="plus-circle"><PlusCircle className='feather'/></span>
-          </Link>
-        </h6>
-      </div>
-    </nav>
+          </h6>
+        </div>
+      </nav>
     </>
   );
 }
--- frontend/src/profile/Profile.js
+++ frontend/src/profile/Profile.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {useForm} from 'react-hook-form'
 import Wrapper from '../component/Wrapper'
 
 export default function Profile() {
+  const {register,handleSubmit, formState:{errors}}= useForm()
+
+  const onSubmit = (data)=>{
+    console.log(data)
+  }
   return (
     <Wrapper>
-        <p>Profile</p>
+      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+        {/*<h1 className='h2 text-muted'>Enter new forms</h1>*/}
+      </div>
+        <p>Profile page</p>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className='form-outline mb-4'>
+              <input type={'text'} {...register("name")} className={'form-control'} />
+            </div>
+            <div className='form-outline mb-4'>
+              <input type={"text"} {...register("email")} className={'form-control'} />
+            </div>
+            <input type={'submit'} className="btn btn-primary btn-block" />
+          </form>
     </Wrapper>
   );
 }
