143d644f27ce78fae130af45b1f04f0f50c06493
ronewa <ronewamavhungu@gmail.com>
2022-02-28 17:18:51 +0200

    time picker

--- frontend/src/pages/NewMedicine.js
+++ frontend/src/pages/NewMedicine.js
@@ -2,6 +2,13 @@ import React,{useState} from 'react';
 import Wrapper from '../component/Wrapper';
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import momentTimeZone from 'moment-timezone';
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import Stack from '@mui/material/Stack';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import TimePicker from '@mui/lab/TimePicker';
+//import moment from 'moment';
 //import {useForm} from 'react-hook-form'
 
 export default function NewMedicine() {
@@ -9,12 +16,21 @@ export default function NewMedicine() {
   const [name,setName]= useState('')
   const [dosage,setDosage]= useState(0)
   const [frequency,setFrequency]= useState(0)
+  const [timeZone,setTimeZone] = useState('')
+  const [value, setValue] = useState(new Date('2022-03-01T00:00:00.000Z'))
+  const [notification,setNotification] = useState()
+  
+  const getTimeZones = function() {
+    return momentTimeZone.tz.names();
+  };
+  const timeZones = getTimeZones()
 
   const Meds = async (e)=>{
     e.preventDefault();
     if (!name || !dosage || !frequency) return;
-    const medicine = { name, dosage, frequency };
+    const medicine = { name, dosage, frequency, timeZone, value, notification };
       const {data} = await axios.post('addmedicine',medicine,{withCredentials:true});
+      console.log(data)
       if(data.msg === 'success'){ toast.success("success data")}
     
   }
@@ -46,6 +62,38 @@ export default function NewMedicine() {
                 Looks good!
               </div>
             </div>
+
+
+            <div className='col-auto'>
+              <label htmlFor="inputState" className="form-label">State</label>
+                <select id="inputState" className="form-select" name="time" onChange={(e)=>setTimeZone(e.target.value)}>
+                  {
+                    timeZones.map((timeZone, i)=>{
+                      return <option value={timeZone} key={i}>{timeZone}</option>
+                    })
+                  }
+                </select>
+            </div>
+            <div className='col-auto'>
+              <label htmlFor='' className='form-label'>Notifications</label>
+              <input type={'text'} id='' className='form-control' onChange={(e)=>setNotification(e.target.value)} />
+            </div>
+
+            <div className='col-auto'>
+              <label className='form-label' htmlFor=''></label>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <Stack spacing={3}>
+                    <TimePicker
+                      label="Notification"
+                      className='form-control'
+                      value={value}
+                      onChange={setValue}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </Stack>
+                </LocalizationProvider>
+            </div>
+
             <div className="col-12">
               <button className="btn btn-primary" type="submit">Save</button>
             </div>
