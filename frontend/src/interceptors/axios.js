b4f9b308278286f79a9f0ded91172b905044d476
ronewa <ronewamavhungu@gmail.com>
2022-05-20 17:44:07 +0200

    account email

--- frontend/src/interceptors/axios.js
+++ frontend/src/interceptors/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:8000/api/';
-//axios.defaults.baseURL = 'https://ronewa-medicine-tracker.herokuapp.com/api'; 
+//axios.defaults.baseURL = 'http://localhost:8000/api/';
+axios.defaults.baseURL = 'https://ronewa-medicine-tracker.herokuapp.com/api'; 
 
 let refresh = false;
 
