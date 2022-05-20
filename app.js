2bbf4ba9e9e129a860ccee7528567cd163230521
ronewa <ronewamavhungu@gmail.com>
2022-05-20 09:50:29 +0200

    make it better

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(mongoSanitize())
 app.use(cookieParser())
 app.use(cors({
     //origin: ['http://localhost:3000'],
-    origin: ['https://ronewa-medicine-tracker.herokuapp.com'],
+    origin: ['https://ronewa-medicine-tracker.herokuapp.com'], 
     credentials: true,
 }));
 
