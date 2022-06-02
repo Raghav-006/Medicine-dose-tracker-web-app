54c09e6b823d03fa33e949b526992342dc7da91f
ronewa <ronewamavhungu@gmail.com>
2022-06-02 17:06:56 +0200

    change..

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -45,8 +45,9 @@ const Login = ()=>{
                                 </label>
                                 <input
                                     type='email'
-                                    className='form-input email-input'
                                     name='email'
+                                    className='form-input email-input'
+                                    placeholder='Enter your email'
                                     value={email}
                                     onChange={(e) => setEmail(e.target.value)}
                                 />
