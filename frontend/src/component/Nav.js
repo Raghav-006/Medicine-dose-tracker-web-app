9dd9af3f6bb0cbb7145fb170b027074f224c690b
ronewa <ronewamavhungu@gmail.com>
2022-06-12 16:08:10 +0200

    heroku

--- frontend/src/component/Nav.js
+++ frontend/src/component/Nav.js
@@ -13,7 +13,7 @@ const Nav = ({user}) =>{
   return (
     <div>
         <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-            <Link to={'/dashboard'} className="navbar-brand col-md-3 col-lg-2 me-0 px-3">{user.avatars}</Link>
+            <Link to={'/dashboard'} className="navbar-brand col-md-3 col-lg-2 me-0 px-3">{user.name}</Link>
             <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
