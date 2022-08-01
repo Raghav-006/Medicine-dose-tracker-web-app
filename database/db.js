e362e8f53404ede3a8ccde2997414a39c3295189
ronewa <ronewamavhungu@gmail.com>
2022-08-01 06:27:12 +0200

    fix connection

--- database/db.js
+++ database/db.js
@@ -1,44 +1,32 @@
-/*const mongoose = require('mongoose');
-try {
-    mongoose.connect(process.env.ATLAS_URI,{
-        useUniFiedTopology: true,
-        useNewUrlParser: true,
-    },()=>{
-        console.log('connected to the database medicationss');
-    })
-} catch (error) {
-    console.log('unable to connect') 
-};*/
 const mongoose = require("mongoose");
 const chalk = require("chalk");
 const log = console.log;
 
 const connectDB = (uri) => {
     try {
-        mongoose.connection.on('connecting', () => {
-            log(chalk.magenta(`connecting to database: ${uri}`))
-        });
-        mongoose.connect(uri, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-        }, (err, db) => {
-            if (err) {
-                return console.log(chalk.bgGreen('unable to connect to database: ' + uri));
-            }
-            if (db) {
-                console.log(chalk.yellowBright.bold("Connected successfully to mongodb server " + uri));
-            }
-        });
-        mongoose.connection.on('disconnected', () => {
-            log(chalk.bgCyanBright(`disconnected to database: ${uri}`))
-        });
-        mongoose.connection.on('reconnected', () => {
-            log(chalk.redBright(`reconnected again to database: ${uri}`))
-        });
+      mongoose.connection.on('connecting', () => {
+        log(chalk.magenta(`connecting to database: ${uri}`))
+      });
+      mongoose.connect(uri, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        autoIndex: false,
+      }, (err, db) => {
+        if (err) {
+          return log(chalk.bgGreen('unable to connect to database: ' + uri + ' ' + err));
+        }
+        if (db) {
+          log(chalk.yellowBright.bold("Connected successfully to mongodb server " + uri));
+        }
+      });
+      mongoose.connection.on('disconnected', () => {
+        log(chalk.bgCyanBright(`disconnected to database: ${uri}`))
+      });
+      mongoose.connection.on('reconnected', () => {
+        log(chalk.redBright(`reconnected again to database: ${uri}`))
+      });
     } catch (error) {
-    console.log(chalk.white('unable to connect to database URl: ' + error));
+      log(chalk.white('unable to connect to database URl: ' + error));
     }
 };
 
