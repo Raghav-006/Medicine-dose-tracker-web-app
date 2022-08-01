9128ac41f4d92f0ebd9c401a48af4152b5d13846
ronewa <ronewamavhungu@gmail.com>
2022-08-01 21:25:37 +0200

    profile card

--- frontend/src/pages/users/ProfileView.js
+++ frontend/src/pages/users/ProfileView.js
@@ -1,11 +1,28 @@
-import react from "react";
-import Wrapper from '../../component/Wrapper';
+import React from "react";
+import { Card, Button } from 'react-bootstrap';
+import Avatar from '@mui/material/Avatar';
+import { deepPurple } from '@mui/material/colors';
+import Pta from '../../images/pta.jpg';
 
-const ProfileView = ()=>{
+const ProfileView = ({avator, name, surname, zipcode, state, city})=>{
     return (
-        <Wrapper>
-            <p>ProfileView </p>
-        </Wrapper>
+        <>
+            <div className="col-md-4">
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={`${avator}`} alt="avator" />
+                <Avatar sx={{ bgcolor: deepPurple[500] }}>{'OP'}</Avatar>
+                <Card.Body>
+                    <Card.Title>{name} {surname}</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+                </Card>
+            </div>
+            <div className="col-md-8">222</div>
+        </>
     )
 }
 export default ProfileView;
